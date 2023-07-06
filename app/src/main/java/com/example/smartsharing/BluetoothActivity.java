079f16cb13dc4c957de053715b544ee57a4be199
isufhasanii <hasanii@bzz.ch>
2023-07-06 11:15:40 +0200

    Commit devicelist & toast for serach have started

--- app/src/main/java/com/example/smartsharing/BluetoothActivity.java
+++ app/src/main/java/com/example/smartsharing/BluetoothActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -69,30 +70,24 @@ public class BluetoothActivity extends AppCompatActivity {
             }
         });
 
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+        deviceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        deviceListView.setAdapter(deviceAdapter);
+        deviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                BluetoothDevice selectedDevice = bluetoothDevices.get(position);
+                connectToDevice(selectedDevice);
             }
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH);
+        });
+    }
+
+    private void discoverBluetoothDevices() {
+        if (bluetoothAdapter.isDiscovering()) {
+            Toast.makeText(this, "Suche läuft bereits", Toast.LENGTH_SHORT).show();
+            return;
         }
+    }
+
 
     }
-}
+
