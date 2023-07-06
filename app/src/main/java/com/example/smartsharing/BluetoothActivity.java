4db0fd90c8776f2dc48f2f0cb6d7dba276adf91e
isufhasanii <hasanii@bzz.ch>
2023-07-06 11:16:17 +0200

    Commit permissioncheck

--- app/src/main/java/com/example/smartsharing/BluetoothActivity.java
+++ app/src/main/java/com/example/smartsharing/BluetoothActivity.java
@@ -82,6 +82,26 @@ public class BluetoothActivity extends AppCompatActivity {
     }
 
     private void discoverBluetoothDevices() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         if (bluetoothAdapter.isDiscovering()) {
             Toast.makeText(this, "Suche läuft bereits", Toast.LENGTH_SHORT).show();
             return;
